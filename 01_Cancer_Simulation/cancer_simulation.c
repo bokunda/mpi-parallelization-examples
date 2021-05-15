50a2fdd3a91dd24d596165f742616ee132f206c4
bokunda <bpiskulic1996@gmail.com>
2021-05-16 01:24:49 +0200

    DoWork implementation & corner row handling

--- 01_Cancer_Simulation/cancer_simulation.c
+++ 01_Cancer_Simulation/cancer_simulation.c
@@ -10,7 +10,7 @@
 
 #define CANCER 0
 #define MIN_CANCER_HEALTH 0
-#define MAX_CANCER_HEALTH 100
+#define MAX_CANCER_HEALTH 101
 #define CANCER_PROB 2
 
 #define MEDECINE 1
@@ -20,15 +20,16 @@
 
 #define TISSUE 2
 #define MIN_TISSUE_HEALTH 0
-#define MAX_TISSUE_HEALTH 10000
+#define MAX_TISSUE_HEALTH 10001
 #define TISSUE_PROB 80
 
 #define PRINT_TYPE 1
 #define PRINT_HEALTH 2
+#define PRINT_ALL 3
 
 #define M 10
 #define N 10
-#define STEPS 10
+#define STEPS 1000
 
 typedef struct Cell {
     int type;
@@ -41,12 +42,18 @@ void myMax(int *invector, int *outvector, int *len, MPI_Datatype type)
     *outvector = *invector > *outvector ? *invector : *outvector;
 }
 
+// Allocate array
+cell* allocateArray(int n)
+{
+    return (cell*)malloc(n * sizeof(cell));
+}
+
 // Allocates a new matrix m x n size
 cell** allocateMatrix(int m, int n)
 {
     cell** matrix;
     matrix = (cell**)malloc(m * sizeof(cell*));
-    for (int i = 0; i < m; i++) matrix[i] = (cell*)malloc(n * sizeof(cell));
+    for (int i = 0; i < m; i++) matrix[i] = allocateArray(n);
     return matrix;
 }
 
@@ -98,13 +105,133 @@ void printMatrix(cell** matrix, int m, int n, int printOption)
             {
                 printf("%d ", matrix[i][j].type);
             }
-            else
+            else if (printOption == PRINT_HEALTH)
             {
                 printf("%d ", matrix[i][j].health);
+            }
+            else
+            {
+                printf("[%d]-%d ", matrix[i][j].type, matrix[i][j].health);
             }
         }
         printf("\n");
     }
+}
+
+// Check if compared cell is a tissue or medecine
+int tryApplyCancerOrMedicine(cell **matrix, int i, int j, cell value)
+{
+    int hasCancerNeighbour = 0;
+
+    if (matrix[i][j].type == TISSUE && value.type == CANCER)
+    {
+        matrix[i][j].health -= value.health;
+
+        if (matrix[i][j].health <= 0)
+        {
+            matrix[i][j].type = CANCER;
+            matrix[i][j].health = rand() % 101;
+        }
+        hasCancerNeighbour = 1;
+    }
+
+    if (matrix[i][j].type == CANCER && value.type == MEDECINE)
+    {
+        matrix[i][j].health -= value.health;
+        
+        if (matrix[i][j].health <= 0)
+        {
+            matrix[i][j].type = TISSUE;
+            matrix[i][j].health = rand() % MAX_TISSUE_HEALTH + MIN_TISSUE_HEALTH;
+        }
+        else
+        {
+            hasCancerNeighbour = 1;
+        }
+
+    }
+
+    return hasCancerNeighbour;
+}
+
+// Main function where we are simulating cells behaviour
+void doWork(cell **matrix, int startM, int endM, int n)
+{
+    int i, j;
+    for (i = startM; i <= endM; i++)
+    {
+        int hasCancerNeighbour = 0;
+        for (j = 0; j < n; j++)
+        {
+            // check left
+            if (j > 0) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i][j-1]);
+            // check right
+            if (j < n) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i][j+1]);
+            // check top
+            if (i > startM) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i-1][j]);
+            // check bottom
+            if (i < endM) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i+1][j]);
+            // check top-left
+            if (j > 0 && i > startM) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i-1][j-1]);
+            // check top-right
+            if (j < n && i > startM) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i-1][j+1]);
+            // check bottom-left
+            if (j > 0 && i < endM) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i+1][j-1]);
+            // check bottom-right
+            if (j < n && i < endM) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, matrix[i+1][j+1]);
+
+            // convert cell into a tissue if there cell is surrounded with tissues
+            if (hasCancerNeighbour == 0 && matrix[i][j].type == MEDECINE)
+            {
+                matrix[i][j].type = TISSUE;
+                matrix[i][j].health = rand() % MAX_TISSUE_HEALTH + MIN_TISSUE_HEALTH;
+            }
+        }
+    }
+}
+
+// Handles edge cases, similar like doWork
+void doWorkEdge(cell **matrix, cell *rowToCompare, int n, int i)
+{
+    int j;
+    int hasCancerNeighbour = 0;
+    for (j = 0; j < n; j++)
+    {
+        // default check
+        hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, rowToCompare[j]);
+
+        // check edge-left
+        if (j > 0) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, rowToCompare[j-1]);
+        // check edge-right
+        if (j < n) hasCancerNeighbour += tryApplyCancerOrMedicine(matrix, i, j, rowToCompare[j+1]);
+
+        if (hasCancerNeighbour == 0 && matrix[i][j].type == MEDECINE)
+        {
+            matrix[i][j].type = TISSUE;
+            matrix[i][j].health = rand() % MAX_TISSUE_HEALTH + MIN_TISSUE_HEALTH;
+        }
+    }
+}
+
+// Finds max cell health
+cell findLocalStrongestCancerCell(cell **matrix, int startM, int endM, int n)
+{
+    int i, j;
+    cell strongest;
+    strongest.type = CANCER;
+    strongest.health = -1;
+
+    for (i = startM; i <= endM; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (matrix[i][j].type == CANCER && matrix[i][j].health > strongest.health)
+            {
+                strongest = matrix[i][j];
+            }
+        }
+    }
+    return strongest;
 }
 
 int main(int argc, char **argv)
@@ -137,6 +264,8 @@ int main(int argc, char **argv)
     cell **matrix;
     matrix = allocateMatrix(M, N);
 
+    cell *bottom_row = allocateArray(N);
+
     if (rank == 0)
     {
         // Allocate matrix
@@ -150,16 +279,48 @@ int main(int argc, char **argv)
         MPI_Bcast(matrix[i], N, mycelltype, 0, MPI_COMM_WORLD);
     }
 
-    // TODO: Define submatrix for each process
-    
+    // Define submatrix for each process
+    int startM = rank * (M / size);
+    int endM = startM + (M / size) - 1;
+
     // Simulate
-    // TODO: Loop through steps
-    // TODO: Do Work - Simulate
-    // TODO: Sync corner rows
-    
+    int step = 0;
+
+    // Loop through steps
+    for (int i = 0; i < STEPS; i++)
+    {
+        // Simulate
+        doWork(matrix, startM, endM, N);
+
+        // Sync corner rows
+        // upper cells
+        if (rank != 0)
+        {
+            MPI_Send(matrix[startM], N, mycelltype, rank - 1, 40, MPI_COMM_WORLD);
+            MPI_Recv(bottom_row, N, mycelltype, rank - 1, 40, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            doWorkEdge(matrix, bottom_row, N, startM);
+        }
+        // lower cells
+        if (rank != size - 1)
+        {
+            MPI_Send(matrix[endM], N, mycelltype, rank + 1, 40, MPI_COMM_WORLD);
+            MPI_Recv(bottom_row, N, mycelltype, rank + 1, 40, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+            doWorkEdge(matrix, bottom_row, N, startM);
+        }
+    }
+
     // Find strongest
-    // TODO: Find local strongest
-    // TODO: Find global strongest with custom redustion
+    // Find local strongest
+    cell strongest = findLocalStrongestCancerCell(matrix, startM, endM, N);
+    printf("[%d]: Strongest local: %d_%d\n", rank, strongest.type, strongest.health);
+
+    // Find global strongest with custom redustion
+    int strongestGlobal; 
+    MPI_Reduce(&strongest.health, &strongestGlobal, 1, MPI_INT, MPI_MY_MAX, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) printf("Strongest global: %d\n", strongestGlobal);
 
     // Cleanup and finish
     MPI_Op_free(&MPI_MY_MAX);
